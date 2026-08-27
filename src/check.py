4934cd6b3fdbccb7f6b357fe20117e528096baa9
Sophia Ye <nf.sophia.ye@gmail.com>
2026-08-26 21:33:13 -0400

    add docstring to the check task script

---
+++ src/check.py
@@ -0,0 +1,9 @@
+"""double checking task status"""
+import ee
+ee.Initialize(project="cerrado-deforestation")
+
+for t in ee.batch.Task.list()[:5]:
+    s = t.status()
+    print(s["state"], "|", s.get("description"), "|", s.get("id"))
+    if s.get("error_message"):
+        print("   error:", s["error_message"])
